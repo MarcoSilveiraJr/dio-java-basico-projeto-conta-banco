c604802cc87e7e994ad73143cd39e44230f3983a
Marco Silveira Jr <marcosilveirajr@hotmail.com>
2024-07-25 00:10:40 -0300

    contaterminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Por favor, digite o número da sua agência: ");
+        String Agencia = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Agora digite o número de sua conta: ");
+        String numeroContaStr = scanner.nextLine();
+        int Numero = Integer.parseInt(numeroContaStr);
+        System.out.println();
+
+        System.out.print("Informe seu nome completo: ");
+        String NomeCliente = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Informe o valor do depósito inicial: ");
+        String saldoStr = scanner.nextLine();
+        double Saldo = Double.parseDouble(saldoStr);
+
+        scanner.close();
+
+        DadosCliente cliente = new DadosCliente(Numero, Agencia, NomeCliente, Saldo);
+
+        System.out.println(cliente.toString());
+        
+
+    
     }
 }
